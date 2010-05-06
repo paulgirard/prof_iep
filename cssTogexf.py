3cc111ffb1d712eec37a17ad9cac7791ff217a90
pom <pom@jiminy-dev.medialab>
2010-05-06 17:36:54 +0200

    initial commit : before version 0.2

---
+++ cssTogexf.py
@@ -0,0 +1,281 @@
+# -*- coding: utf-8 -*-
+import re
+
+
+verbose=0
+export_gexf=1
+export_dot=1
+
+if export_gexf :
+	import gexf
+
+
+def supprime_accent(ligne):
+        """ supprime les accents du texte source """
+        accents = { 'a': ['à', 'ã', 'á', 'â'],
+                    'e': ['é', 'è', 'ê', 'ë'],
+                    'i': ['î', 'ï'],
+                    'u': ['ù', 'ü', 'û','Ü'],
+                    'o': ['ô', 'ö','Ö'],
+                    'c' : ['ç'] }
+        for (char, accented_chars) in accents.iteritems():
+            for accented_char in accented_chars:
+                ligne = ligne.replace(accented_char, char)
+        return ligne
+
+
+
+def loadCategory(file):
+# load a csv 
+# colomun 0 : category name
+# column 1	: group1 name
+# column 2	: group2 name
+# column 3	: ID
+# column 4	: entity name
+# returns dict [id]=["cat","group1","group2","name"]
+	
+	cat=""
+	group1=group2=""
+	nbline=1
+	codes={}
+	lines = file.readlines()
+	for line in lines:
+		try :
+			data=line.split(";")
+			#print str(nbline)+":"+data[0]
+			if not data[0]=="" :
+				cat=data[0]
+			else : 
+				group1=data[1] if not data[1]=="" else group1
+				group2=data[2] if not data[2]=="" else group2
+				id=data[3] if len(data)>=4 else ""
+				if not id=="" :
+					name=re.sub("[\r\n]","",data[4]) if len(data)>=5 else ""
+					codes[id]=[supprime_accent(cat),supprime_accent(group1),supprime_accent(group2),supprime_accent(name),id]
+		except Exception as e :
+			print "error line "+str(nbline)+" : "+line
+			print e
+		nbline+=1
+
+	return codes
+	
+def loadProf(file):
+# load a csv 
+# columns :
+# NOM	DISCI	FORMATION1	F2	F3	DIPLOME1	D2	PRO1	T1	PRO2	T2	PRO3	T3	PRO4	T4	PRO5	T5	PRO6	T6	PRO7	T7	EXTRA1	T2	EXTRA2	T2	EXTRA3	T3	EXTRA4	T4	EXTRA5	T5	EXTRA6	T6
+#
+# this function will extract :  
+# colomun 0 :  name
+# column 2	: formation 
+# column 3	: f2
+# column 4	: f3
+# column 7	: profession 
+# column 9	: f2
+# column 11	: f3
+# column 13	: f3
+# column 15	: f3
+# column 17	: f3
+# column 19	: f3
+	
+	
+	profs=[]
+	lines = file.readlines()
+	nbline=1
+	for line in lines:
+		try :
+			data=line.split(";")
+			
+			name=data[0]
+			formations=[d for d in data[2:4] if not d in ("999","") ]
+			professions=[d for i,d in enumerate(data[7:20]) if not d in ("999","") and i%2==0]
+			if len(professions)>0 : #len(formations)>0 or 
+				profs.append([supprime_accent(name),map(supprime_accent,formations),map(supprime_accent,professions),nbline])
+			else :
+				print " no formation or no profession"+name+str(len(formations))+" "+str(len(professions)) 
+		except Exception as e:
+			print " error line "+str(nbline)+" : "+line
+			print e
+		nbline+=1
+
+	return profs	
+	
+
+def getDotLinkString(node1,node2,weight,label) :
+	#return '"'+node1+'"->"'+node2+'",weight='+weight+',label="'+label+'";\n'
+	return ''+node1+'->'+node2+'&sep;'+weight+'&sep;'+label+'\n'
+
+def generateProfToProfGraph(profs,professionsCat,file_prefix):
+	print "graph prof to prof"
+	dotString=""
+	idEdge=0
+	
+	if export_gexf :
+		gexf_file=gexf.Gexf("Paul Girard medialab Sciences Po","IEP professers linked by common institutions "+file_prefix)
+		graph=gexf_file.addGraph("undirected","static")
+		
+		for name,formations,professions,id in profs :
+			graph.addNode(str(id),name)
+		
+		
+	for prof1, prof2 in [(p1,p2) for i,p1 in enumerate(profs) for p2 in profs[i:] if p1!=p2] :		weight=0		labels=[]
+		# pour toutes les professions du prof 1		for profID in prof1[2] :
+			# si la profession est présente dans celle du prof2
+			if profID in prof2[2] :				# on incrémente le poids du lien et on ajoute la profession au label
+				weight+=1				labels.append(professionsCat[profID][3] if not professionsCat[profID][3]=="" else professionsCat[profID][2])		# si on a trouvé des professions communes
+		if weight>0 :
+			# on ajoute un lien
+			if export_gexf  :				graph.addEdge(idEdge,str(prof1[3]),str(prof2[3]),weight=str(weight),label=" | ".join(labels))
+			if export_dot :
+				dotString+=getDotLinkString(str(prof1[3])+"-"+prof1[0],str(prof2[3])+"-"+prof2[0],str(weight)," | ".join(labels))
+			idEdge+=1
+	
+	if export_gexf  :
+		file=open("profiep_profToprof_"+file_prefix+".gexf","w+")
+		gexf_file.write(file)
+	
+	if export_dot :
+		file=open("profiep_profToprof.dot","w+")
+		file.write("digrpah output {\n"+dotString+"}")
+		
+		
+################### INST TO INST #########################
+#
+#
+##########################################################
+def generateInstToInstGraph(profs,professionsCat,file_prefix):
+	print "graph inst to inst"
+	dotString=""
+	idEdge=0
+	
+	if export_gexf :
+		gexf_file=gexf.Gexf("Paul Girard medialab Sciences Po","Institutions linked by common IEP professers "+file_prefix)
+		graph=gexf_file.addGraph("undirected","static")
+		idAttInstCat2=graph.addNodeAttribute("cat2","","String")
+		idAttInstCat1=graph.addNodeAttribute("cat1","","String")
+		
+		for cat,group1,group2,name,id in professionsCat.values() :
+			n=graph.addNode(str(id),name if not name=="" else group2)
+			n.addAttribute(idAttInstCat2,group2)
+			n.addAttribute(idAttInstCat1,group1)
+	
+	
+	for inst1,inst2 in [(inst1,inst2) for i,inst1 in enumerate(professionsCat.values()) for inst2 in professionsCat.values()[i:] if inst1!=inst2] :		weight=0		labels=[]
+		idinst1=inst1[4]
+		idinst2=inst2[4]
+		
+		# pour toutes les professions du prof 1
+		for prof in profs :			if idinst1 in prof[2] and idinst2 in prof[2] :
+			# si inst1 et inst 2 sont présentes dans les professions du prof				# on incrémente le poids du lien et on ajoute le prof au label
+				weight+=1				labels.append(prof[0])		# si on a trouvé des professeurs communs
+		if weight>0 :
+			# on ajoute un lien
+			node1=inst1[3] if not inst1[3]=="" else inst1[2]
+			node2=inst2[3] if not inst2[3]=="" else inst2[2]
+
+			if export_gexf :				graph.addEdge(idEdge,inst1[4],inst2[4],weight=str(weight),label=" | ".join(labels))
+			if export_dot :
+				dotString+=getDotLinkString(node1,node2,str(weight)," | ".join(labels))
+			idEdge+=1
+				
+	if export_gexf  :
+		file=open("profiep_instToinst_"+file_prefix+".gexf","w+")
+		gexf_file.write(file)
+	
+	if export_dot :
+		file=open("profiep_instToinst.dot","w+")
+		file.write("digrpah output {\n"+dotString+"}")
+
+################### PROF TO INST #########################
+#
+#
+##########################################################
+
+def generateProfInstitutionGraph(profs,professionsCat,file_prefix):
+	print "graph prof to inst"
+	dotString=""
+	idEdge=0
+	
+
+		
+	if export_gexf :
+		gexf_file=gexf.Gexf("Paul Girard medialab Sciences Po","Institutions and professers IEP "+file_prefix)
+		graph=gexf_file.addGraph("undirected","static")
+		idAttType=graph.addNodeAttribute("type","professer","String")
+		idAttInstCat=graph.addNodeAttribute("cat2","","String")
+		idAttInstCat1=graph.addNodeAttribute("cat1","","String")
+
+		
+		for cat,group1,group2,name,id in professionsCat.values() :
+			n=graph.addNode("inst_"+str(id),name if not name=="" else group2)
+			
+			n.addAttribute(idAttType,"institution")
+			
+			n.addAttribute(idAttInstCat,group2)
+			n.addAttribute(idAttInstCat1,group1)
+			
+		for name,formations,professions,id in profs :
+			n=graph.addNode("prof_"+str(id),name)
+			n.addAttribute(idAttType,"professer")
+	
+	for prof in profs :
+		# pour toutes les professions du prof 1		for instID in prof[2] :
+			if export_gexf :				graph.addEdge(idEdge,str("prof_")+str(prof[3]),str("inst_")+str(instID))
+				idEdge+=1	
+			if export_dot :
+				profession=professionsCat[instID][3] if not professionsCat[instID][3]=="" else professionsCat[instID][2]
+				dotString+=getDotLinkString(str(prof[3])+"-"+prof[0],profession,"1","")	
+	if export_gexf :
+		file=open("profiep_profToinst_"+file_prefix+".gexf","w+")
+		gexf_file.write(file)
+	
+	if export_dot :
+		file=open("profiep_profToinst.dot","w+")
+		file.write("digrpah output {\n"+dotString+"}")	
+			
+# load categories
+
+# profession
+file=open("profession.csv")
+professionsCat=loadCategory(file)
+#print professions
+if verbose :
+	for id,vals in professionsCat.iteritems() :
+		print id+"|"+"|".join(vals)
+
+# formation
+
+# extra
+
+
+
+for year in ("1970","2008") :
+	# load prof
+	file=open(year+".csv")
+	profs=loadProf(file)
+	print year+": number profs loaded :"+str(len(profs))
+	#print professions
+	if verbose :
+		for name,formations,professions,id in profs :
+			print name+"|"+"|".join([professionsCat[p][3] if not professionsCat[p][3]=="" else professionsCat[p][2] for p in professions])
+	# let's gexf this
+	
+	# node = professers
+	# links = shared institutions
+	# nodes
+	key=lambda prof:prof[0]
+	profs=sorted(profs, key=key )
+	
+	
+	generateProfInstitutionGraph(profs,professionsCat,year)
+	generateProfToProfGraph(profs,professionsCat,year)
+	generateInstToInstGraph(profs,professionsCat,year)
+
+
+
+
+
+
+
+
+
+
---
+++ gexf.py
@@ -0,0 +1,322 @@
+# -*- coding: utf-8 -*-
+
+from lxml  import etree
+from datetime import  *
+ 
+ 
+ # missing features : 
+ # load, validate, modify existing gexf file
+ # slices / phylogeny / ...
+ 
+ # evolution ideas :
+ # add display stats on graph composition when exportingto xml
+ # add anti-paralell edges test
+ # add a test based on existing example from gexf.net
+ # add modification accessors like setStart ...
+ # factorize attribute managment by creating an attribute class
+ # add a test code utility to check that code will not use _variable outside objects
+ 
+class Gexf :
+
+	def __init__(self,creator,description):
+		self.creator=creator
+		self.description=description
+		self.graphs=[]
+		self.xmlns="http://www.gephi.org/gexf/1.1draft"
+		self.xsi="http://www.w3.org/2001/XMLSchema-instance"
+		self.schemaLocation="http://www.gephi.org/gexf/1.1draft http://gephi.org/gexf/1.1draft.xsd"
+		self.version="1.1"
+	
+	def addGraph(self,type,mode):
+		g = Graph(type,mode)
+		self.graphs.append(g)
+		return g
+ 	
+ 	def getXML(self):
+ 		gexfXML = etree.Element("gexf",xmlns=self.xmlns,version=self.version)
+ 		gexfXML.set("xmlnsxsi",self.xsi)
+ 		gexfXML.set("xsischemaLocation",self.schemaLocation)
+		meta = etree.SubElement(gexfXML, "meta")
+		meta.set("lastmodified",datetime.now().isoformat())
+		etree.SubElement(meta, "creator").text=self.creator		
+		etree.SubElement(meta, "description").text=self.description
+		for graph in self.graphs :
+			gexfXML.append(graph.getXML())
+			
+		return gexfXML
+ 		
+ 	def write(self,file):
+ 		file.write(etree.tostring(self.getXML(),pretty_print=True,encoding='utf-8'))
+ 		self.print_stat()
+ 	
+ 	def print_stat(self) :
+ 		for graph in self.graphs :
+ 			graph.print_stat()
+
+ 	
+ 
+class Graph :
+
+	
+	
+	
+	def __init__(self,type,mode,start="",end="") :
+		
+		# control variable
+		self.authorizedType=("directed","undirected")
+		self.authorizedMode=("dynamic","static")
+		self.defaultType="directed"
+		self.defaultMode="static"
+		
+		
+		if type in self.authorizedType :
+			self.type=type
+		else :
+			self.type=self.defaultType
+		if mode in self.authorizedMode :
+			self.mode=mode
+		else :
+			self.mode=self.defaultMode
+		
+	
+		self.start=start
+		
+		self.end = end
+		
+		
+		self._nodesAttributes={}
+		self._edgesAttributes={}
+		self._nodes={}
+		self._edges={}
+		
+	def addNode(self,id,label,start="",end="") :
+		self._nodes[id]=Node(self,id,label,start,end)
+		return self._nodes[id]
+	
+	def nodeExists(self,id) :
+		if id in self._nodes.keys():
+			return 1
+		else :
+			return 0
+		
+	def addEdge(self,id,source,target,weight="",start="",end="",label="") :
+		self._edges[id]=Edge(self,id,source,target,weight,start,end,label)
+		return self._edges[id]
+	
+	def addNodeAttribute(self,title,defaultValue,type="integer",mode="static") :
+		# add to NodeAttributes
+		# generate id
+		id=len(self._nodesAttributes)
+		self._nodesAttributes[id]={"title":title , "default":defaultValue, "mode":mode, "type":type}		# modify Nodes with default
+		#: bad idea and unecessary
+		#for node in self._nodes.values():
+		#	node.addAttribute(id,defaultValue)
+
+		return id
+	
+	def addDefaultAttributesToNode(self,node) :
+		# add existing nodesattributes default values
+		for id,values in self._nodesAttributes.iteritems() :
+			node.addAttribute(id,values["default"])
+			
+	def checkNodeAttribute(self,id,value,start,end):
+		# check conformity with type is missing
+		if id in self._nodesAttributes.keys() :
+			if self._nodesAttributes[id]["mode"]=="static" and ( not start=="" or not end=="") : 
+				raise Exception("attribute "+str(id)+" is static you can't specify start or end dates. Declare Attribute as dynamic")
+			return 1		
+		else :
+			raise Exception("attribute id unknown. Add Attribute to graph first")
+
+		
+	def addEdgeAttribute(self,title,defaultValue,type="integer",mode="static"):
+		# add to NodeAttributes
+		# generate id
+		id=len(self._edgesAttributes)
+		self._edgesAttributes[id]={"title":title, "default":defaultValue, "mode":mode, "type":type } 		# modify Nodes with default
+		#for edge in self._edges.values():
+		#	edge.addAttribute(id,defaultValue)
+			
+			
+	def addDefaultAttributesToEdge(self,edge) :
+		# add existing nodesattributes default values
+		for id,values in self._edgesAttributes.iteritems() :
+			edge.addAttribute(id,values["default"])
+			
+	def checkEdgeAttribute(self,id,value,start,end):
+		# check conformity with type is missing
+		if id in self._edgesAttributes.keys() :
+			if self._edgesAttributes[id]["mode"]=="static" and ( not start=="" or not end=="") : 
+				raise Exception("attribute "+str(id)+" is static you can't specify start or end dates. Declare Attribute as dynamic")
+			return 1		
+		else :
+			raise Exception("attribute id unknown. Add Attribute to graph first")
+
+	
+	def getXML(self) :
+		# return lxml etree element
+		graphXML = etree.Element("graph",defaultedgetype=self.type,mode=self.mode)
+		attributesXMLNodeDynamic = etree.SubElement(graphXML, "attributes")
+		attributesXMLNodeDynamic.set("class","node")
+		attributesXMLNodeDynamic.set("mode","dynamic")
+		attributesXMLNodeStatic = etree.SubElement(graphXML, "attributes")
+		attributesXMLNodeStatic.set("class","node")
+		attributesXMLNodeStatic.set("mode","static")
+		
+		for id,value in self._nodesAttributes.iteritems() :
+			if value["mode"]=="static" :
+				attxml=attributesXMLNodeStatic
+			else :
+				attxml=attributesXMLNodeDynamic
+			
+			attributeXML=etree.SubElement(attxml, "attribute")
+			attributeXML.set("id",str(id))
+			attributeXML.set("title",value["title"])
+			attributeXML.set("type",value["type"])
+			etree.SubElement(attributeXML, "default").text=value["default"]
+		
+		attributesXMLEdgeDynamic = etree.SubElement(graphXML, "attributes")
+		attributesXMLEdgeDynamic.set("class","edge")
+		attributesXMLEdgeDynamic.set("mode","dynamic")
+		attributesXMLEdgeStatic = etree.SubElement(graphXML, "attributes")
+		attributesXMLEdgeStatic.set("class","edge")
+		attributesXMLEdgeStatic.set("mode","static")
+		
+		for id,value in self._edgesAttributes.iteritems() :
+			if value["mode"]=="static" :
+				attxml=attributesXMLEdgeStatic
+			else :
+				attxml=attributesXMLEdgeDynamic
+			
+			attributeXML=etree.SubElement(attxml, "attribute")
+			attributeXML.set("id",str(id))
+			attributeXML.set("title",value["title"])
+			attributeXML.set("type",value["type"])
+			etree.SubElement(attributeXML, "default").text=value["default"]
+		
+		
+		
+		nodesXML = etree.SubElement(graphXML, "nodes")
+		for node in self._nodes.values() :
+			nodesXML.append(node.getXML())
+			
+		edgesXML = etree.SubElement(graphXML, "edges")
+		for edge in self._edges.values() :
+			edgesXML.append(edge.getXML())
+			
+		return graphXML
+		
+	def print_stat(self):
+		print self.type+" "+self.mode+" "+self.start+" "+self.end
+ 		print "number of nodes : "+str(len(self._nodes))
+ 		print "number of edges : "+str(len(self._edges))
+		
+		
+class Node :
+
+	def __init__(self,graph,id,label,start="",end="",pid="") :
+		self.id =id 
+		self.label=label
+		self.start=start
+		self.end=end
+		self.pid=pid
+		self._attributes=[]
+		self._graph=graph
+		# add existing nodesattributes default values : bad idea and unecessary
+		#self._graph.addDefaultAttributesToNode(self)
+		
+	def addAttribute(self,id,value,start="",end="") :
+		if self._graph.checkNodeAttribute(id,value,start,end) :
+			self._attributes.append({"id":id,"value":value,"start":start,"end":end})
+			
+			
+	def getXML(self) :
+		# return lxml etree element
+		try :
+			nodeXML = etree.Element("node",id=self.id,label=str(self.label))
+			if not self.start == "":
+				nodeXML.set("start",self.start)
+			if not self.end == "":
+				nodeXML.set("end",self.end)
+			if not self.pid == "":
+				nodeXML.set("pid",self.pid)
+			
+			attributesXML = etree.SubElement(nodeXML, "attvalues")
+			for atts in self._attributes :
+				attributeXML=etree.SubElement(attributesXML, "attvalue")
+				attributeXML.set("for",str(atts["id"]))
+				
+				attributeXML.set("value",atts["value"])
+				
+				if not atts["start"]=="" :
+					attributeXML.set("start",atts["start"])
+				if not atts["end"]=="" :
+					attributeXML.set("end",atts["end"])
+			
+			return nodeXML
+		except Exception as e:
+			print self.label
+			print self._attributes	
+			print e
+			exit()	
+	
+class Edge :
+
+	def __init__(self,graph,id,source,target,weight="",start="",end="",label="") :
+		self.id =id
+		self._graph=graph
+		
+		
+		if self._graph.nodeExists(source) :
+			self._source=source
+		else :
+			raise Exception("source "+source+" node unknown, add nodes to graph first")
+			
+		if self._graph.nodeExists(target) :
+			self._target=target
+		else:
+			raise Exception("target "+target+" node unknown, add nodes to graph first")	
+					
+		self.start=start
+		self.end=end
+		self.weight=weight
+		self.label=label
+		self._attributes=[]
+		
+		# add existing nodesattributes default values : bad idea and unecessary
+		#self._graph.addDefaultAttributesToEdge(self)
+		
+		
+	def addAttribute(self,id,value,start="",end="") :
+		if self._graph.checkEdgeAttribute(id,value,start,end) :
+			self._attributes.append({"id":id,"value":value,"start":start,"end":end})
+		
+	
+	def getXML(self) :
+		# return lxml etree element
+		try :
+			edgeXML = etree.Element("edge",id=str(self.id),source=str(self._source),target=str(self._target))
+			if not self.start == "":
+				edgeXML.set("start",self.start)
+			if not self.end == "":
+				edgeXML.set("end",self.end)
+			if not self.weight == "":
+				edgeXML.set("weight",str(self.weight))
+			if not self.label == "":
+				edgeXML.set("label",str(self.label))
+			
+			attributesXML = etree.SubElement(edgeXML, "attvalues")
+			for atts in self._attributes :
+				attributeXML=etree.SubElement(attributesXML, "attvalue")
+				attributeXML.set("for",str(atts["id"]))
+				attributeXML.set("value",atts["value"])
+				if not atts["start"]=="" :
+					attributeXML.set("start",atts["start"])
+				if not atts["end"]=="" :
+					attributeXML.set("end",atts["end"])
+			return edgeXML
+		except Exception as e:
+			print self._source+" "+self._target	
+			print e
+			exit()	
+			
